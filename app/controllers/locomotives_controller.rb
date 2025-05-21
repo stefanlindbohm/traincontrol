6d07dc0e7f064ea08646acf2835fdc0f5f6087bf
Stefan Lindbohm <stefan@lindbohm.se>
2025-05-21 21:35:00 +0200

    Add function keys to locomotive view

--- app/controllers/locomotives_controller.rb
+++ app/controllers/locomotives_controller.rb
@@ -24,11 +24,15 @@ class LocomotivesController < ApplicationController
 
   def locomotive_params
     params.require(:locomotive)
-          .permit(:speed, :direction, :lights)
+          .permit(:speed, :direction, :lights, :f1, :f2, :f3, :f4)
           .to_hash
           .transform_keys(&:to_sym)
           .tap { _1[:speed] = _1[:speed].to_i if _1.key?(:speed) }
           .tap { _1[:direction] = _1[:direction].to_sym if _1.key?(:direction) }
           .tap { _1[:lights] = _1[:lights].to_i.positive? if _1.key?(:lights) }
+          .tap { _1[:f1] = _1[:f1].to_i.positive? if _1.key?(:f1) }
+          .tap { _1[:f2] = _1[:f2].to_i.positive? if _1.key?(:f2) }
+          .tap { _1[:f3] = _1[:f3].to_i.positive? if _1.key?(:f3) }
+          .tap { _1[:f4] = _1[:f4].to_i.positive? if _1.key?(:f4) }
   end
 end
