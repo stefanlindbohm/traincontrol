5a8ad30da3ad54b1ddfeb013981800cda750ec4e
Stefan Lindbohm <stefan@lindbohm.se>
2026-04-27 12:20:27 +0200

    Sort turnout names naturally (taking numbers into account)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def natural_sort_by(collection, &block)
+    collection.sort_by do |element|
+      block.call(element).scan(/(\d+)|(\D+)/).map { |i, s| s.presence || i.to_i }
+    end
+  end
 end
